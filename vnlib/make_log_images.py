fixed log scaling and transparency in make_log_images.py

vnlib/make_log_images.py
@@ -20,10 +20,52 @@ import datetime
 
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 
 import vnlib
 import colormap
+
+def plot_with_log(title, data, log_data, cm):
+    plt.clf()
+    plt.cla()
+    plt.subplot(211, xticks=[], yticks=[])
+    #    plt.subplot(221)
+    plt.imshow(data, cmap=cm, vmin=0.0, vmax=60.0)
+    # plt.imshow(log_mrms, cmap=cm, vmin=0.01, vmax=np.log10(60.0)))
+    cax1 = plt.axes([0.65, 0.53, 0.075, 0.36])
+    cb1 = plt.colorbar(cax=cax1)
+    cb1.set_label('Rain rate [mm/hr]', rotation=-90, color='k', labelpad=20)
+    # cb.minorticks_on()
+    cb1.set_ticks([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60], update_ticks=True)
+    # cb.set_ticks([0,10,20,30,40,50,60], update_ticks=True)
+    cb1.update_ticks()
+    plt.subplot(212, xticks=[], yticks=[])
+    #    plt.subplot(223)
+    plt.imshow(log_data, cmap=cm, vmin=-1, vmax=np.log10(100.0))
+    # plt.imshow(gpm_data, cmap=cm, vmin=0.0, vmax=60.0)
+    # plt.imshow(log_gpm, cmap=cm, vmin=0.01, vmax=np.log10(60.0))
+    # plt.imshow(np.random.random((100, 100)) * 60, cmap=cm)
+
+    cax2 = plt.axes([0.65, 0.1, 0.075, 0.36])
+    cb2 = plt.colorbar(cax=cax2)
+    cb2.set_label('Log10(Rain rate) log10[mm/hr]', rotation=-90, color='k', labelpad=20)
+    # cb.minorticks_on()
+    cb2.set_ticks([-1, 0, 1, 2], update_ticks=True)
+    # cb.set_ticks([0,10,20,30,40,50,60], update_ticks=True)
+    cb2.update_ticks()
+
+    plt.subplots_adjust(bottom=0.1, right=0.7, top=0.9)
+    plt.figtext(0.5, 0.95, title, horizontalalignment='center')
+    # cax = plt.axes([0.75, 0.1, 0.075, 0.8])
+
+    # cax1 = plt.axes([0.75, 0.1, 0.075, 0.4])
+    # cb = plt.colorbar(cax=cax1)
+    # cb.set_label('Rain rate [mm/hr]', rotation=-90, color='k', labelpad=20)
+    # #cb.minorticks_on()
+    # cb.set_ticks([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60], update_ticks=True)
+    # # cb.set_ticks([0,10,20,30,40,50,60], update_ticks=True)
+    # cb.update_ticks()
+
 
 def main():
 
@@ -37,6 +79,9 @@ def main():
     #testing
     mrms_filename = '/data/capri_test_data/VN/mrms_geomatch/2020/GRtoDPR.KABR.200312.34295.V06A.DPR.NS.1_21.nc.gz.mrms.bin'
 
+    parsed_by_dot=mrms_filename.split('.')
+    site=parsed_by_dot[1]
+    date=parsed_by_dot[2]
     # load mrms, gpm, and footprint .bin files int MRMSToGPM class variable MRMSMatch
     # assumes filename format used in the Java VN matchup program for MRMS data
     # and also assumes that the footprint and GPM images (.bin files) are in the same directory
@@ -73,53 +118,68 @@ def main():
     bw = []
     ind = 0
     for row in colormap.colormap.data:
-        entry = (float(row[0]) / 255.0, float(row[1]) / 255.0, float(row[2]) / 255.0)
-        colors.append(entry)
-        entry = (float(ind) / 255.0, float(ind) / 255.0, float(ind) / 255.0)
-        bw.append(entry)
+        if ind == 0:
+            entry = (0.0,0.0,0.0,0.0)
+            colors.append(entry)
+            bw.append(entry)
+        else:
+            entry = (float(row[0]) / 255.0, float(row[1]) / 255.0, float(row[2]) / 255.0, 1.0)
+            colors.append(entry)
+            entry = (float(ind) / 255.0, float(ind) / 255.0, float(ind) / 255.0, 1.0)
+            bw.append(entry)
         ind = ind + 1
 
     # color
-    cm = LinearSegmentedColormap.from_list('Colors', colors, N=256)
-    plt.subplot(211)
+#    cm = LinearSegmentedColormap.from_list('Colors', colors, N=256)
+    cm = ListedColormap(colors, N=256)
     #    plt.imshow(np.random.random((100, 100))*60, cmap=plt.cm.BuPu_r)
     #plt.imshow(np.random.random((100, 100)) * 60, cmap=cm)
+
 
     # clamp values at 60
     b = np.where(mrms_data > 60)
     mrms_data[b]=60
+
     b = np.where(mrms_data > 0)
     min_mrms = np.min(mrms_data[b])
     b = np.where(mrms_data < min_mrms)
-    mrms_data[b]=min_mrms
+    #mrms_data[b]=min_mrms
+    mrms_data[b]=-9999
     print ("max mrms", np.max(mrms_data))
     print ("min mrms", np.min(mrms_data))
+
     b = np.where(gpm_data > 60)
     gpm_data[b]=60
+
     # need to set up RGB values directly, not using colormaps so I can set missing to black
-    missing = np.where(gpm_data < 0)
+#    missing = np.where(gpm_data < 0)
     b = np.where(gpm_data > 0)
     min_gpm = np.min(gpm_data[b])
     b = np.where(gpm_data < min_gpm)
-    gpm_data[b]=min_gpm
+    #gpm_data[b]=min_gpm
+    gpm_data[b]=-9999
     print ("max gpm", np.max(gpm_data))
     print ("min gpm", np.min(gpm_data))
 
-#    plt.imshow((mrms_data/60.0), cmap=cm)
-    plt.imshow(np.log10(mrms_data), cmap=cm)
-    plt.subplot(212)
-    plt.imshow(np.log10(gpm_data), cmap=cm)
-    #plt.imshow(np.random.random((100, 100)) * 60, cmap=cm)
 
-    plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9)
-    cax = plt.axes([0.75, 0.1, 0.075, 0.8])
-    cb = plt.colorbar(cax=cax)
-    cb.set_label('Rain rate [mm/hr]', rotation=-90, color='k', labelpad=20)
-    #cb.minorticks_on()
-    cb.set_ticks([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60], update_ticks=True)
-    # cb.set_ticks([0,10,20,30,40,50,60], update_ticks=True)
-    cb.update_ticks()
-    plt.savefig('colorbar.png')
+    log_gpm = np.copy(gpm_data)
+    log_mrms = np.copy(mrms_data)
+
+    a = np.where(log_gpm > 0)
+    log_gpm[a] = np.log10(log_gpm[a])
+    a = np.where(log_mrms > 0)
+    log_mrms[a] = np.log10(log_mrms[a])
+
+#    plt.imsave('mrms_log.png', log_mrms, cmap=cm, vmin=0.01, vmax=np.log10(60.0))
+    plt.imsave('mrms_log.png', log_mrms, cmap=cm, vmin=-1, vmax=np.log10(100.0))
+    plt.imsave('mrms.png', mrms_data, cmap=cm, vmin=0.0, vmax=60.0)
+    plt.imsave('gpm_log.png', log_gpm, cmap=cm, vmin=-1, vmax=np.log10(100.0))
+    plt.imsave('gpm.png', gpm_data, cmap=cm, vmin=0.0, vmax=60.0)
+
+    plot_with_log('MRMS ' + site +' ' + date, mrms_data, log_mrms, cm)
+    plt.savefig('mrms_fig.png',dpi = (200))
+    plot_with_log('GPM ' + site +' ' + date, gpm_data, log_gpm, cm)
+    plt.savefig('gpm_fig.png',dpi = (200))
 
     # # Now we can loop through MRMSMatch class MRMS gridded GPMFootprint image
     # to retrieve VN query information for each matching MRMS value