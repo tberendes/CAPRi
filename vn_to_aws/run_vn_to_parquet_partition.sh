c93a6e104d4993f7568c988d860a502070ce6fae
tberendes <berendt@uah.edu>
2023-07-25 15:32:54 -0500

    Fixes for vn_to_aws dprgmi

--- vn_to_aws/run_vn_to_parquet_partition.sh
+++ vn_to_aws/run_vn_to_parquet_partition.sh
@@ -7,6 +7,8 @@ source ./v-env/bin/activate
 # syntax:  python vn_to_parquet_partition.py <path to root of netcdf files> <path to root of output parquet directory> <start YYMMDD> <end YYMMDD>
 # example
 python vn_to_parquet_partition.py /data/gpmgv/netcdf/geo_match/GPM/2ADPR/FS_Ku/V07A/2_4 /data/gpmgv/athena 160101 230630
+# after FS_Ku, run mrms matched version, will overwrite some files with MRMS matched versions.
+#python vn_to_parquet_partition.py /data/gpmgv/mrms/netcdf/geo_match/GPM/2ADPR/FS_Ku/V07A/2_4 /data/gpmgv/athena 160101 230630
 # run DPRGMI FS and NS
 # example
 python vn_to_parquet_partition.py /data/gpmgv/netcdf/geo_match/GPM/2BDPRGMI/V07A/2_4 /data/gpmgv/athena 190101 230630
